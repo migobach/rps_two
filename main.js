b94c0b0d8f0ace88ae234dbb7e5953c4257007f0
Michael Bach <migobach@gmail.com>
2018-05-15 22:12:13 -0600

    add scores

--- main.js
+++ main.js
@@ -10,10 +10,11 @@ var computerNum = 0
 var rps = ['rock', 'paper', 'scissors']
 var userWin = 0
 var userLose = 0 
+// var plays = 0
 
   function startGame() {
     computerNum = Math.floor(Math.random()*rps.length)
-    // console.log("something")
+    
     if (computerNum === 0) {
       "The computer chose rock"
     } else if (computerNum === 1) {
@@ -25,6 +26,7 @@ var userLose = 0
 
   $('.imgbutton').on('click', function(){
     startGame()
+    // plays++
     userChoice = $(this).data().type
     console.log(userChoice)
     $('#player_choice').text('You chose: ' + userChoice)
@@ -34,23 +36,30 @@ var userLose = 0
     winner(userChoice, computerNum)
   })
 
-  function winner(userChoice, computerNum){
+  function winner(userChoice, computerNum, plays){
     if ((userChoice === 'rock' && computerNum === 2) ||
     (userChoice === 'paper' && computerNum === 0) ||
     (userChoice === 'scissors' && computerNum === 1)) {
       userWin++
+      // userWinPerc = (userWin / plays) * 100
+      // debugger
       $('#results').text('You win! Congratulation')
-      $('#wins').text('Your total wins: ' + userWin)
+      $('#wins').text('Your total wins: ' + userWin /* + ', you win ' + userWinPerc + "% of the time."*/ )
     } else if ((userChoice === 'rock' && computerNum === 1) ||
     (userChoice === 'paper' && computerNum === 2) ||
     (userChoice === 'scissors' && computerNum === 0)) {
       userLose++
+      // userLosePerc = (userLose / plays * 100)
       $('#results').text('You lose..... sucker.') 
-      $('#loses').text('Your total loses: ' + userLose)
+      $('#loses').text('Your total loses: ' + userLose /*+ ', you lose' + userLosePerc + "% of the time."*/)
     } else {
       $('#results').text("It's a tie. Try again.")
     }
+
   }
 
-  new startGame
+  // $('#new').on('click', fuction() {
+  //   var = new startGame
+  // })
+  
 })
